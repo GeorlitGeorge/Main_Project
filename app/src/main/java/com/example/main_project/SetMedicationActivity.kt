16b121a7741fbef2056e6be85f017c1079a94798
GeorlitGeorge <georlitgeorge22@gmail.com>
2024-02-21 00:44:39 +0530

    Initial commit

---
+++ app/src/main/java/com/example/main_project/SetMedicationActivity.kt
@@ -0,0 +1,16 @@
+package com.example.main_project
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SetMedicationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_medication)
+
+
+
+        }
+    }
+
